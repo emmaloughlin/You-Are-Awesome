48e3aad0c416cc92726377c16e7422eb26fa1e7d
Emma Loughlin <loughlem@bc.edu>
2018-09-04 08:59:54 -0400

    2.8 Complete

---
+++ MyPlayground.playground/Contents.swift
@@ -0,0 +1,13 @@
+import UIKit
+
+var str = "Hello, playground"
+print(str)
+str = "Hello, Developer!"
+print(str)
+
+let message1 = "You Are Awesome!"
+var message2 = "You Are Great!"
+let message3 = "You Are Fantastic!"
+
+message2 = "You Da Bomb"
+
